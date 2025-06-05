f513e243620d21320f8de401beb6862a87159b91
Sudharsan D S <105963347+Sudharsan2618@users.noreply.github.com>
2025-06-05 10:52:07 +0530

    api

--- main.py
+++ main.py
@@ -21,7 +21,7 @@ from flask import Flask
 from flask_cors import CORS
 
 app = Flask(__name__)
-CORS(app,origins="*")
+CORS(app, origins=["https://www.companion-lms.com"])
 
 # Register blueprints
 app.register_blueprint(auth_bp)
