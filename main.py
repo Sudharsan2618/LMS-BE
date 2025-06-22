1c33ffa3fd101b30b4273e51f9e97012ea504ba5
Sudharsan D S <105963347+Sudharsan2618@users.noreply.github.com>
2025-06-22 23:37:18 +0530

    course content generate completed

--- main.py
+++ main.py
@@ -24,8 +24,8 @@ from flask import Flask
 from flask_cors import CORS
 
 app = Flask(__name__)
-# CORS(app, origins=["https://www.companion-lms.com"])
-CORS(app, origins=["http://localhost:3000"])
+CORS(app, origins=["https://www.companion-lms.com"])
+# CORS(app, origins=["http://localhost:3000"])
 
 # Register blueprints
 app.register_blueprint(auth_bp)
